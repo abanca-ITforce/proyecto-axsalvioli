dea733730c73c29b37105408a74aeffd08b2285b
Assis Salvioli <assissalvioli@gmail.com>
2019-12-02 16:53:12 +0100

    npm angular y material

---
+++ explorador/src/app/layout/layout.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavShellComponent } from './nav-shell/nav-shell.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+
+
+@NgModule({
+  declarations: [NavShellComponent],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class LayoutModule { }
